f3ebfc5bd08c1ade3f2073e9ba98b99f7d62bde2
Rusith <rusith@mail.com>
2021-10-10 09:26:24 +0530

    up and running

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-export function puls(num1: number, num2: number) {
-  return 0 //ssssss
+import Hapi from "@hapi/hapi"
+import mongoose from "mongoose"
+import config from "config"
+
+const init = async () => {
+  await mongoose.connect(config.get("db.url"))
+
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost"
+  })
+
+  await server.start()
+  console.log("Server running on %s", server.info.uri)
 }
+
+process.on("unhandledRejection", (err) => {
+  console.log(err)
+  process.exit(1)
+})
+
+init().catch(console.error)
